7ab1014387dcaf5775c37cf1fd365a9e12712609
Sofia Toro <sofie.torch@outlook.com>
2022-11-07 22:56:52 -0400

    implement cubit in establishments map

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mikhuy/app/app.dart';
+import 'package:mikhuy/home/cubit/google_maps_cubit.dart';
 import 'package:mikhuy/home/view/google_maps_builder.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,7 +12,12 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          GoogleMapsBuilder(),
+          BlocProvider<GoogleMapsCubit>(
+            create: (context) => GoogleMapsCubit()
+              ..verifyLocationPermission()
+              ..getEstablisments(),
+            child: const GoogleMapsBuilder(),
+          ),
           Positioned(
             bottom: 0,
             child: TextButton(
