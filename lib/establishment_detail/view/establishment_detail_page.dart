afa79251cf0215c40778b6b0831910137df4a04e
jhonatang7 <gabriel24332@hotmail.com>
2022-11-08 13:20:48 -0400

    Add base file for establishment detail page

---
+++ lib/establishment_detail/view/establishment_detail_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EstablishmentDetailPage extends StatelessWidget{
+  const EstablishmentDetailPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
