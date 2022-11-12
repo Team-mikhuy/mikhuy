8af19fa4bf802d90998434a3bb2ad7cf0a1212f0
JhonatanGabrielPenarandaAndia <gabriel24332@hotmail.com>
2022-11-12 01:13:29 -0400

    Add file products_list.dart

---
+++ lib/establishment_detail/view/products_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+class ProductsList extends StatelessWidget {
+  const ProductsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
+
+class _ProductsListView extends StatelessWidget {
+  const _ProductsListView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _ProductsListItem extends StatelessWidget {
+  const _ProductsListItem();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
